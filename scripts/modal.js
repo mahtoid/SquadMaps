4740c88e59ddca72fb5cef8b2435155e980e545e
Pablo Ferrer <100330656@alumnos.uc3m.es>
2019-08-05 13:10:19 +0200

    vehicles added
    Al Basrah complete

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,6 +4,11 @@ var map_dict = {
 	Al_Basrah_Insurgency_v1: ["GB", "200", "FV432 APC", "FV432 APC", "FV510 IFV", "FV510 UA IFV", "FV510 UA IFV", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Transport Truck", "Delayed 06:00", "INS", "600", "Armored Technical DshK", "Armored Technical SPG-9", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Technical DshK", "Technical DshK", "Technical DshK", "Technical SPG-9", "Technical SPG-9", "ZU-23 Anti-Air Truck"],
 	Al_Basrah_Invasion_v1: ["GB", "150", "FV432 APC", "FV432 APC", "FV510 IFV", "FV510 UA IFV", "FV510 UA IFV", "FV510 UA IFV", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Transport Truck", "Delayed 06:00", "INS", "800", "Armored Technical DshK", "Armored Technical SPG-9", "Armored Technical SPG-9", "BMP-1 IFV", "BRDM-2 Scout Car", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Technical DshK", "Technical SPG-9", "ZU-23 Anti-Air Truck"],
 	Al_Basrah_Invasion_v2: ["GB", "150", "FV4034 MBT", "FV432 APC", "FV432 APC", "FV510 IFV", "FV510 UA IFV", "FV510 UA IFV", "FV510 UA IFV", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Transport Truck", "Delayed 06:00", "INS", "800", "Armored Technical DshK", "Armored Technical SPG-9", "Armored Technical SPG-9", "BMP-1 IFV", "BRDM-2 Scout Car", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "T-62 MBT", "Technical DshK", "Technical SPG-9", "Transport Technical", "ZU-23 Anti-Air Truck"],
+	Al_Basrah_RAAS_v1: ["MIL", "300", "BMP-1 ZU-23 Anti Air APC", "BRDM-2 Scout Car", "BRDM-2 Spandrel", "Delayed 30:00", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "T-62 MBT", "Delayed 30:00", "Technical DshK", "Technical DshK", "Technical DshK", "Technical DshK", "Technical SPG-9", "Transport Technical", "Delayed 06:00", "Ural 375 Logistics Truck", "Ural 375 Logistics Truck", "ZU-23 Anti-Air Truck", "INS", "300", "Armored Technical DshK", "Armored Technical DshK", "Armored Technical SPG-9", "BMP-1 ZU-23 Anti Air APC", "BRDM-2 Spandrel", "Delayed 30:00", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "T-62 MBT", "Delayed 30:00", "Technical DshK", "Technical DshK", "Transport Technical", "Delayed 06:00", "ZU-23 Anti-Air Truck"],
+	Al_Basrah_Skirmish_v1: ["GB", "100", "HX60 Logistics Truck", "HX60 Logistics Truck", "INS", "100", "Armored Technical DshK", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle"],
+	Al_Basrah_Skirmish_v2: ["MIL", "100", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Transport Technical", "ZU-23 Anti-Air Truck", "INS", "100", "Logistics Technical", "Logistics Technical", "Minsk Motorcycle", "Minsk Motorcycle", "Transport Technical", "ZU-23 Anti-Air Truck"],
+	Al_Basrah_TC_v1: ["GB", "400", "FV510 UA IFV", "FV510 UA IFV", "FV510 UA IFV", "HX60 Transport Truck", "Delayed 06:00", "HX60 Logistics Truck", "HX60 Logistics Truck", "HX60 Logistics Truck", "FV432 APC", "FV432 APC", "INS", "400", "T-62 MBT", "Delayed 30:00", "BRDM-2 Spandrel", "Delayed 30:00", "BMP-1 ZU-23 Anti Air APC", "Transport Technical", "Delayed 06:00", "Armored Technical SPG-9", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Armored Technical DshK", "Armored Technical DshK", "Technical DshK", "Technical DshK", "ZU-23 Anti-Air Truck", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle"],
+	Al_Basrah_TC_v2: ["US", "400", "M1126 M2HB APC", "M1126 M2HB APC", "M2A2 IFV", "M-ATV M2HB", "M-ATV M2HB", "M939 Transport Truck", "Delayed 06:00", "M939 Logistics Truck", "M939 Logistics Truck", "INS", "400", "T-62 MBT", "Delayed 20:00", "BRDM-2 Spandrel", "BMP-1 ZU-23 Anti Air APC", "Transport Technical", "Delayed 06:00", "Armored Technical SPG-9", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Logistics Technical", "Armored Technical DshK", "Armored Technical DshK", "Technical DshK", "Technical DshK", "ZU-23 Anti-Air Truck", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle", "Minsk Motorcycle"],
 }
 
 function view_vehicles (map)
@@ -14,8 +19,10 @@ function view_vehicles (map)
 	var ul_blufor = document.getElementById("list_blufor");
 	var ul_opfor = document.getElementById("list_opfor");
 
+	document.getElementById("layer_name").innerHTML = map.replace(/_/g, " ");
+
 	map_data = map_dict[map].slice(0);
-	map_data.reverse();
+	map_data = map_data.reverse();
 
 	var team_blufor = map_data.pop();
 	document.getElementById("team_blufor").innerHTML = team_blufor;
@@ -50,21 +57,14 @@ function view_vehicles (map)
 	do
 	{
 		var element = map_data.pop();
+
 		var li = document.createElement("li");
 		var img = document.createElement("img");
 		var p = document.createElement("p");
 
-		if (element.includes("Delayed"))
-		{
-			small = document.createElement("SMALL");
-			small.innerHTML = element;
-			ul_blufor.lastChild.lastChild.appendChild(small);
-			continue;
-		}
-
 		switch (element)
 		{
-			case "BMP-1 ZU-23 Anti-Air APC":
+			case "BMP-1 ZU-23 Anti Air APC":
 				img.src = "img/map_antiair.png";
 				p.innerHTML = element;
 				break;
@@ -157,32 +157,40 @@ function view_vehicles (map)
 			case "CA":
 				document.getElementById("team_opfor").innerHTML = element;
 				document.getElementById("img_opfor").src = "img/flag_CA.png";
-				break;
+				continue;
 			case "GB":
 				document.getElementById("team_opfor").innerHTML = element;
 				document.getElementById("img_opfor").src = "img/flag_GB.png";
-				break;
+				continue;
 			case "INS":
 				document.getElementById("team_opfor").innerHTML = element;
 				document.getElementById("img_opfor").src = "img/flag_INS.png";
-				break;
+				continue;
 			case "MIL":
 				document.getElementById("team_opfor").innerHTML = element;
 				document.getElementById("img_opfor").src = "img/flag_MIL.png";
-				break;
+				continue;
 			case "RUS":
 				document.getElementById("team_opfor").innerHTML = element;
 				document.getElementById("img_opfor").src = "img/flag_RUS.png";
-				break;
+				continue;
 			case "USA":
 				document.getElementById("team_opfor").innerHTML = element;
 				document.getElementById("img_opfor").src = "img/flag_USA.png";
-				break;
-
+				continue;
 			default:
-				break;
+				if (element.includes("Delayed"))
+				{
+					var small = document.createElement("SMALL");
+					small.innerHTML = element;
+					ul_blufor.lastChild.lastChild.appendChild(small);
+				}
+				else
+				{
+					console.error("Could not identify element: " + element);
+				}
+				continue;
 		}
-
 		li.appendChild(img);
 		li.appendChild(p);
 		ul_blufor.appendChild(li);
@@ -191,23 +199,16 @@ function view_vehicles (map)
 	document.getElementById("tickets_opfor").innerHTML = map_data.pop() + " tickets";
 
 	var element = map_data.pop();
+
 	while (typeof element !== 'undefined')
 	{
 		var li = document.createElement("li");
 		var img = document.createElement("img");
 		var p = document.createElement("p");
 
-		if (element.includes("Delayed"))
-		{
-			small = document.createElement("SMALL");
-			small.innerHTML = element;
-			ul_blufor.lastChild.lastChild.appendChild(small);
-			continue;
-		}
-
 		switch (element)
 		{
-			case "BMP-1 ZU-23 Anti-Air APC":
+			case "BMP-1 ZU-23 Anti Air APC":
 				img.src = "img/map_antiair.png";
 				p.innerHTML = element;
 				break;
@@ -296,15 +297,23 @@ function view_vehicles (map)
 				img.src = "img/map_truck_transport.png";
 				p.innerHTML = element;
 				break;
-
 			default:
-				break;
+				if (element.includes("Delayed"))
+				{
+					var small = document.createElement("SMALL");
+					small.innerHTML = element;
+					ul_opfor.lastChild.lastChild.appendChild(small);
+					element = map_data.pop();
+				}
+				else
+				{
+					console.error("Could not identify element: " + element);
+				}
+				continue;
 		}
-
 		li.appendChild(img);
 		li.appendChild(p);
 		ul_opfor.appendChild(li);
-
 		element = map_data.pop();
 	}
 
